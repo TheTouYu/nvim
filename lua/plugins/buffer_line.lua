a0a8b361700a447f1c6b7250893916937ce96090
jack.li <bindo1118@Zacks-Macbook.lan>
2025-01-17 20:22:43 +0800

    25.1.17

---
+++ lua/plugins/buffer_line.lua
@@ -0,0 +1,19 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    {
+      "L",
+      function()
+        vim.cmd("bnext " .. vim.v.count1)
+      end,
+      desc = "Next buffer",
+    },
+    {
+      "H",
+      function()
+        vim.cmd("bprev " .. vim.v.count1)
+      end,
+      desc = "Previous buffer",
+    },
+  },
+}
